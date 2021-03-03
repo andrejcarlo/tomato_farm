fd4534bae051a443dd15f8835589b6ea2b8326e2
Andrej <andrejcarlop@gmail.com>
2021-03-03 15:38:22 +0100

    basic functions firebase

--- farm-api/functions/index.js
+++ farm-api/functions/index.js
@@ -1,9 +1,41 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const express = require("express");
+const cors = require("cors");
+
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const app = express();
+
+app.get("/", async (req, res) => {
+    const snapshot = await admin.firestore().collection("users").get();
+  
+    let users = [];
+    snapshot.forEach((doc) => {
+      let id = doc.id;
+      let data = doc.data();
+  
+      users.push({ id, ...data });
+    })
+
+    res.status(200).send(JSON.stringify(users));
+});
+
+app.post("/", async (req, res) => {
+    const user = req.body;
+
+    await admin.firestore().collection("users").add(user);
+
+    // status 201 states created
+    res.status(201).send();
+});
+
+
+exports.user = functions.https.onRequest(app);
+
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+
