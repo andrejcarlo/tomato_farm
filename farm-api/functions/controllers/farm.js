d5622c81c90034aba9d9c735eeb9b0280cb2c529
Andrej <andrejcarlop@gmail.com>
2021-03-04 09:29:49 +0100

    changed structure of db and finished CRUD operations

--- farm-api/functions/controllers/farm.js
+++ farm-api/functions/controllers/farm.js
@@ -6,13 +6,13 @@ const cors = require("cors");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-const db = admin.firestore()
+const db = admin.firestore();
 const farmApp = express();
 
 farmApp.use(cors({origin:true}))
 
 farmApp.get("/", async (req, res) => {
-    const snapshot = await db.collection("tomato-farm").get();
+    const snapshot = await db.get();
   
     let farm_data = [];
     snapshot.forEach((doc) => {
@@ -25,15 +25,69 @@ farmApp.get("/", async (req, res) => {
     res.status(200).send(JSON.stringify(farm_data));
 });
 
-farmApp.post("/", async (req, res) => {
+farmApp.post("/:collection", async (req, res) => {
+  try {
     const farm_data = req.body;
 
-    await db.collection("tomato-farm").add(farm_data);
+    // /* eslint-disable no-await-in-loop */
+    // for (const data of farm_data) {
+    //   await db.collection(req.params["collection"]).add(data);
+    // }
+    // add farm_data snapshot
+    await db.collection(req.params["collection"]).add(farm_data);
 
     // status 201 states created
-    res.status(201).send();
+    res.status(201).send("Successfully updated!");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
 
+farmApp.get("/:collection", async (req, res) => {
+  try {
+    const snapshot = await db.collection(req.params["collection"]).get();
+  
+    let farm_data = [];
+    snapshot.forEach((doc) => {
+      let id = doc.id;
+      let data = doc.data();
+  
+      farm_data.push({ id, ...data });
+    })
+
+    // status 201 states created
+    res.status(200).send(JSON.stringify(farm_data));
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+
+farmApp.delete("/:collection/:id", async (req,res) => {
+  try {
+
+    await db.collection(req.params["collection"]).doc(req.params.id).delete()
+
+    // status 201 states created
+    res.status(200).send("Delete successful!");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+})
+
+
+farmApp.put("/:collection/:id", async (req, res) => {
+  try {
+    const body = req.body;
+  
+    await db.collection(req.params["collection"]).doc(req.params.id).update(body);
+
+    // status 201 states created
+    res.status(200).send("Update successful!");
+  } catch (error) {
+    res.status(400).send(error.message)
+  }
+});
 
 exports.farm = functions.https.onRequest(farmApp);
 
